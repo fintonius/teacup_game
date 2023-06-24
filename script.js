2d07b5acc2bc5c7c4ed3640d2cd1638194546741
fintonius <fintonius.mcginty@gmail.com>
2023-06-24 13:07:24 +0100

    add new functions & mousevents to handle user input

--- script.js
+++ script.js
@@ -5,73 +5,91 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH =  canvas.width = 600;
 CANVAS_HEIGHT = canvas.height = 400;
 
-
-
 const counter = document.getElementById('counter');
 counter.textContent = 0;
-let frameCounter = document.getElementById('frameCounter');
-let frameCount = 0;
-let testClick = 0;
-let heightClick = 0;
-let power = 0;
-let angle = 0;
-let clicked = false;
-let move = false;
-let startPosition = [20, 180];
+const userInput = document.getElementById('userInput');
+let g = 9.8;
+let velocity = 500;
+let angle = 50;
+let velocityCounter = document.getElementById('velocity');
+velocityCounter.textContent = 'Velocity = 0'
+let angleCounter = document.getElementById('angle');
+angleCounter.textContent = 'Angle = 0'
 
-canvas.addEventListener('mousedown', (e) => {
-    testClick = e.offsetX;
-    angleClick = e.offsetY;
-    console.log('click', e.offsetY)
-    clicked = true;
-    counter.textContent = 0;
-});
-
-canvas.addEventListener('mousemove', (e) => {
-   if (clicked == true) {
-     power = testClick - e.offsetX;
-     angle = angleClick + e.offsetY;
-     
-    counter.textContent = power + ' & ' + angle; 
-   };
+const frame = document.getElementById('userInput');
+const dragger = document.getElementById('userBall');
+dragger.addEventListener('mousedown', (element) => {
+    let startX = element.x;
+    let startY = element.y;
+    console.log(startX);
+    frame.onmousemove = dragBall;
+    frame.onmouseup = endInput;
+    function dragBall (e) {
+        let currentX = e.x;
+        let currentY = e.y;
+        dragger.style.right = (startX - currentX) + 'px';
+        velocity = (startX - currentX);
+        dragger.style.bottom = (startY - currentY) + 'px';
+        angle = (startY - currentY);
+        angleCounter.textContent = 'Angle = ' + angle;
+        velocityCounter.textContent = 'Velocity = ' + velocity;
+        console.log('test velocity', velocity);
+        console.log('test angle', angle);
+    }
+    function endInput() {
+        frame.onmousemove = null;
+        dragger.onmouseup = null;
+        dragger.style.right = 0 + 'px';
+        dragger.style.bottom = 0 + 'px';
+    }
 })
 
-canvas.addEventListener('mouseup', (e) => {
-    console.log('angle =', angle);
-    move = true;
-    clicked = false;
+
+//how to make an element that can be dragged to determine a number and angle?
+
+// //this calculates the distance and trajectory of the throw based on the 
+// //velocity and angle given by the user
+// function movement(v, a) {
+//     //determine the curve of the trajectory by plotting the object's position
+//     //at different time intervals along the parabolic path.
     
-})
-
-
-
-function movement() {
-
-}
+//     //1.split velocity into its horizontal, vx, and vertical, vy, components &
+//     //calculate the initial vertical and horizontal velocitys
+//     let vy = v * Math.sin(a);
+//     let vx = v * Math.cos(a);
+//     console.log ('testing vy & vx. vy = ', vy, 'vx=', vx);
+//     //2.calculate time(t) of flight
+//     let t = (2 * vy)/g
+//     console.log('testing t =', t);
+//     //3.calculate max height, h, reached by the object
+//     let h = (vy * vy)/(2*g);
+//     console.log('testing h =', h);
+//     //4.calculate the horizontal distance, d, traveled by the object
+//     let d = vx * t;
+//     console.log('testing d =', d);
+//     //5.calculate the range, r, which is the horizontal distance
+//     //traveled by the object when it returns to the same height as launch point
+//     let r = 2 * d;
+//     console.log('testing r =', r);
+//     console.log('testing movement function. r.floor =', Math.floor(r)/100, 'h.floor =', Math.floor(h)/100);
+// }
 
 const teabag = {
-    x: startPosition[0],
-    y: startPosition[1],
+    x: 10,
+    y: 300,
      update() {           
-        // if (this.x >= power + 50) {
-        //     move = false;
-        // } else if (move == true) {       
-        //     // console.log(teabag.y) 
-        //         this.x += 15;
-        //         counter.textContent = this.x;
-        //         if (this.y < angle) {
-        //             this.y += 2;
-        //         } else if (this.y >angle) {
-        //             this.y -= 2;
-        //             console.log
-        //         }
-        //     }
-            // (this.y <= angle) ? this.y += 2 : this.y -= 2;
+       if (this.x < velocity && this.y > angle) {
+        this.x++;
+        this.y--;
+       } else if (this.x < velocity && this.y <= angle) {
+        this.x++;
+        this.y++;
+        console.log('testing y', this.y);
+       }
      },
      draw() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 10, 0, 2 * Math.PI);
-        
+        ctx.arc(this.x, this.y, 10, 0, 2 * Math.PI);        
         ctx.stroke();
      },
 }
@@ -81,8 +99,10 @@ function animate() {
         teabag.draw();
         teabag.update();
         
+        
     //     frameCount++
     // frameCounter.textContent = frameCount;
     requestAnimationFrame(animate);
 }
 animate();
+
