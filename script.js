f29c45452e172bbfe165426e41c29438da9bc805
fintonius <fintonius.mcginty@gmail.com>
2023-07-07 17:04:12 +0100

    add collision detection to the cup and canvas edges

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ dragger.addEventListener('mousedown', (element) => {
         dragger.style.right = (startX - currentX) + 'px';
         velocity = Math.floor(startX - currentX)/100;
         dragger.style.bottom = (startY - currentY) + 'px';
-        angle = (startY - currentY);
+        angle = (startY - currentY) * -1;
         angleCounter.textContent = 'Angle = ' + angle;
         velocityCounter.textContent = 'Velocity = ' + velocity;
         console.log('test velocity', velocity);
@@ -64,19 +64,29 @@ function movement(v, a) {
     console.log('test v = ', v, 'test a=', a )
     vy = v * Math.sin(a);
     vx = v * Math.cos(a);
+
+    if(vy < 0) vy * -1;
+    if(vx < 0) vx * -1;
     console.log ('testing vy & vx. vy = ', vy, 'vx=', vx);
     //2.calculate time(t) of flight
      t = (2 * vy)/g
+    if(t < 0) t * -1;
     console.log('testing t =', t);
+
     //3.calculate max height, h, reached by the object
     h = (vy * vy)/(2*g) * 1000;
+    
+    if(h < 0) h * -1;
     console.log('testing h =', h);
     //4.calculate the horizontal distance, d, traveled by the object
      d = (vx * t) * 5000;
+    
+    if (d<0) d * -1;
     console.log('testing d =', d);
     //5.calculate the range, r, which is the horizontal distance
     //traveled by the object when it returns to the same height as launch point
      r = 2 * d;
+     if(r < 0) r = r * -1;
     console.log('testing r =', r);
 }
 
@@ -86,6 +96,8 @@ function movement(v, a) {
 const teabag = {
     x: 10,
     y: 300,
+    width: 10,
+    height: 10,
     currentY: 0,
     maxY: CANVAS_HEIGHT/2 + angle,
     angle: angle,
@@ -96,57 +108,42 @@ const teabag = {
 
         
         // this.angle += this.angleSpeed;
-       if (this.x < r && this.y > 0) {
+        if (this.x + this.width  >= CANVAS_WIDTH) {
+            this.x = CANVAS_WIDTH - this.width;
+            this.y += velocity * 3;
+            if (this.y + this.height >= CANVAS_HEIGHT) {
+                this.y = CANVAS_HEIGHT - this.height;
+               } 
+           } else if (this.x < r && this.y > 0) {
         this.x += velocity * 5;
-        // this.y = Math.sin(this.x * 0.1) * angle;
-        // this.y -= this.curve * Math.sin(this.angle);
-        this.y--;
-        // this.startY += this.maxY - this.currentY;
-        // console.log('testing y', this.y);
-       
+        this.y -= velocity;      
        } else if(this.x >= r && this.y < CANVAS_HEIGHT - 10) {
-        // console.log('this is working');
-        // this.y -= this.curve * Math.sin(this.angle);
-        // this.y-= velocity  * 5;
-        // this.y += this.curve * Math.sin(this.angle);
-        this.y++;     
+        this.y += velocity;     
         this.x += velocity * 5; 
-        // this.y += velocity * 2;
-        // this.y += this.curve * Math.sin(this.angle);
-        // this.angle += this.angleSpeed;
-        // this.y = Math.sin(this.x * 0.1) * angle;
        } else if (this.x === r) {
-        this.x = this.x;
+            this.x = this.x;
+            
        }
 
-       if(this.x > cup.x + cup.width ||
-        this.x + this.width < cup.x ||
-        this.y > cup.y + cup.height ||
-        this.y + this.height < cup.y)
-        { 
-            console.log('BANG!!!');
-            this.collide();
-        } else {
-            console.log('no collision');
- 
-        }
+      
 
      },
      draw() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 10, 0, 2 * Math.PI);        
+        // ctx.rect(this.x, this.y, this.width, this.height)
+        ctx.arc(this.x, this.y, this.width, 0, 2 * Math.PI);        
         ctx.stroke();
      },
      collide() {
-        this.x = cup.x;
+        alert('you scored!')
      }
 }
 
 const cup = {
     x: 900,
-    y: 385,
+    y: 285,
     width: 55,
-    height: 150,
+    height: 155,
     draw() {
         ctx.beginPath();
         ctx.rect(this.x, this.y, this.width, this.height);
@@ -159,22 +156,23 @@ function animate() {
         teabag.draw();
         cup.draw();
         teabag.update();
+        if(cup.x  > teabag.x + teabag.width -6 ||
+            cup.x + cup.width -6 < teabag.x ||
+            cup.y + 6 > teabag.y + teabag.height ||
+            cup.y + cup.height + 6 < teabag.y)
+            { 
+                console.log('no collision');
+            } else {
+                console.log('BANG!!!');
+                teabag.collide();
+
+                
+            }
         requestAnimationFrame(animate);
 };
 
-// this.angle = 0;
-//         this.angleSpeed = Math.random() * 0.2;
-//         this.curve = Math.random() * 7;
-//     }
-//     update(){
-//         this.x -= this.speed;
 
-//         //this is to create a wave pattern that the enemy follows on the y axis
-//         //to create up & down movement
-
-//         if(this.x + this.width <0) this.x = canvas.width;
-//         //animate sprites
-//         if (gameFrame % this.flapSpeed == 0){
-//         this.frame > 4 ? this.frame = 0 : this.frame++;
-//         }
-//     }
+console.log('cup x and width', cup.x, cup.width);
+console.log('cup.x + width', (cup.x + cup.width));
+console.log('teabag x and width', teabag.x, teabag.width);
+console.log('teabag.x + width', (teabag.x + teabag.width));
